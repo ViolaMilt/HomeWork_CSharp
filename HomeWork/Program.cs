489b37a3870e600003ffda6952355cb3293c9059
Viola <viola-doc@yandex.ru>
2022-02-28 22:21:03 +0300

    Заголовки

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Введите число 1");
+﻿Console.WriteLine("Задача 1");
+Console.WriteLine("Введите число 1");
 int a = Convert.ToInt32 (Console.ReadLine());
 Console.WriteLine("Введите число 2");
 int b = Convert.ToInt32 (Console.ReadLine());
