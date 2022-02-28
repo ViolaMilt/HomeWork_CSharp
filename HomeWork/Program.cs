f43f432de1d97fd65bf72584b5506f134c80d789
Viola <viola-doc@yandex.ru>
2022-02-28 21:54:04 +0300

    Задача 1

---
+++ HomeWork/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите число 1");
+int a = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int b = Convert.ToInt32 (Console.ReadLine());
+
+if (a == b)
+{
+Console.WriteLine(" Числа равны ");
+}
+ else
+ {
+if (a > b)
+{
+     Console.WriteLine("Большее число - " + a );
+     Console.WriteLine("Меньшее число - " + b );
+}
+else
+{
+     Console.WriteLine("Большее число - " + b);
+     Console.WriteLine("Меньшее число - " + a);
+}
+}
+
+
+
